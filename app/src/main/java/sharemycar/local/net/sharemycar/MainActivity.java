922910b0201a811c2f95b3b1873069c816605d1a
coronelFernando <fernandohatake10@gmail.com>
2018-03-17 16:32:03 -0700

    Initial commit

---
+++ app/src/main/java/sharemycar/local/net/sharemycar/MainActivity.java
@@ -0,0 +1,13 @@
+package sharemycar.local.net.sharemycar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
